ead38052122cb78f8a1c6e47b66c05b2d49cb069
gabZ-devPB <gabrielpereirabezerra70@gmail.com>
2026-01-05 15:25:36 -0300

    Update amostra.py

--- amostra.py
+++ amostra.py
@@ -3,3 +3,6 @@ import pandas as pd
 df_viagens = pd.read_csv('arquivos/2025_Viagem.csv', encoding='Windows-1252', sep=';', decimal=',')
 amostra = df_viagens.sample(5000, random_state=42)
 amostra.to_csv("viagens_amostra.csv", index=False)
+
+# Utilizado apenas para teste
+# faz uma amostra dos dados oficiais
